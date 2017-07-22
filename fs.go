8d4e71fb570e9693c0d8079092e729b77c5052c5
Darren Yim <darrenyxj@gmail.com>
2017-07-22 09:52:43 +0800

    added type DirD. modification of type Dir from net/http package

---
+++ fs.go
@@ -0,0 +1,46 @@
+package http
+
+import (
+	"errors"
+	"path/filepath"
+	"strings"
+	"os"
+	"path"
+	"net/http"
+)
+
+// A DirD implements FileSystem using the native file system restricted to a
+// specific directory tree.
+//
+// If a file is not found in DirD.Dir, it will serve a default file specified by
+// DirD.Def
+//
+// While the FileSystem.Open method takes '/'-separated paths, a Dir's string
+// value is a filename on the native file system, not a URL, so it is separated
+// by filepath.Separator, which isn't necessarily '/'.
+//
+// An empty Dir is treated as ".".
+type DirD struct {
+	Dir string
+	Def string
+}
+
+func (d DirD) Open(name string) (http.File, error) {
+	if filepath.Separator != '/' && strings.ContainsRune(name, filepath.Separator) ||
+		strings.Contains(name, "\x00") {
+		return nil, errors.New("http: invalid character in file path")
+	}
+	dir := string(d.Dir)
+	if dir == "" {
+		dir = "."
+	}
+	f, err := os.Open(filepath.Join(dir, filepath.FromSlash(path.Clean("/"+name))))
+	if err != nil {
+		fDef, errDef := os.Open(filepath.Join(dir, filepath.FromSlash(path.Clean("/"+d.Def))))
+		if errDef != nil {
+			return nil, errDef
+		}
+		return fDef, nil
+	}
+	return f, nil
+}
